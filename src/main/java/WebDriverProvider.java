adeae997e3ff772adbe0e8d08f68d54109b573e2
spasynkov <s.pasynkov@gmail.com>
2018-03-22 18:38:30 +0200

    moved driver init to constructor

--- src/main/java/WebDriverProvider.java
+++ src/main/java/WebDriverProvider.java
@@ -2,10 +2,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebDriverProvider {
-	private static WebDriver driver = new ChromeDriver();
+	private static WebDriver driver;
 
 
 	private  WebDriverProvider() {
+		driver = new ChromeDriver();
 	}
 	public static WebDriver getDriver(){
 		return driver;
